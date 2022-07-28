4f5f1a480c489512dc57c46e3da4b0c3f8ff7dee
dfer <df_business@qq.com>
2022-07-28 23:08:51 +0800

    123

--- index.php
+++ index.php
@@ -8,4 +8,22 @@ $Address = new Dfer\Tools\Address;
 $common::print($common::about());
 $common::print($Address->provinceAbbreviation('湖北省'));
 $common::print($Address->getChinaChar(5));
-$common::print($Address->randAddress());
+$common::print($Address->randAddress(true));
+
+$str='{
+    "RECORDS": [
+        {
+            "id": "110101001",
+            "name": "东华门街道",
+            "name_traditional": "東華門街道",
+            "name_en": "",
+            "parent_id": "110101",
+            "type": "0",
+            "sort": "0",
+            "type_name": "",
+            "other_name": "",
+            "name_format": ""
+        }
+    ]
+}';
+$common::print($common->mysqlJsonToArray($str));
--- src/Common.php
+++ src/Common.php
@@ -28,6 +28,22 @@ class Common
     public static function print($str=null)
     {
         echo $str.PHP_EOL;
-    }    
+    }
    
+    /**
+     * 把mysql导出的json文本拼接成数组字符串
+     **/
+    public static function mysqlJsonToArray($str=null)
+    {
+		$arr=json_decode($str);
+		$item=$arr->RECORDS;
+		// var_dump($item);
+		
+		$name=[];
+		foreach ($item as $key => $value) {
+		$name[]=$value->name;	
+		}		
+		$result=sprintf('["%s"]',join('","',$name));
+		return $result;
+    }
 }
