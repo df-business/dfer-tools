1c5df4b5e5292d51790cbd5a98ff3a7a0cdbf7b0
dfer <df_business@qq.com>
2022-10-23 23:03:11 +0800

    v2.3

--- src/Ws/Modules/FileMonitor.php
+++ src/Ws/Modules/FileMonitor.php
@@ -7,15 +7,25 @@ use Workerman\Lib\Timer;
 
 /**
  * 文件监控组件
+ * 
  * https://www.workerman.net/doc/workerman/components/file-monitor.html
+ * windows系统不支持reload，无法使用此监控服务。
+ * 只有在debug模式下才生效，daemon下不会执行文件监控（为何不支持daemon模式见下面说明）。
+ * 只有在Worker::runAll运行后加载的文件才能热更新，或者说只有在onXXX回调中加载的文件才能热更新。
+ * 如果开发者确实需要daemon模式开启文件监控及自动更新，可以自行更改代码，将Worker::$daemonize部分的判断去掉即可。
  **/
 class FileMonitor
 {
     public function __construct()
     {
-        global $last_mtime;
+        global $last_mtime;        
+        
         // 检查当前目录
-        $monitor_dir = realpath(__DIR__);
+        // $monitor_dir = realpath(__DIR__);
+        
+        // 检查'/app/command'
+        $root=app()->getRootPath();
+        $monitor_dir = $root."/app/command/";
         
         // worker
         $worker = new Worker();
