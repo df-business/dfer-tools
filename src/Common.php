52c5e46f99b7b7b4fa0828d2bb68c7b0c10700b6
dfer <df_business@qq.com>
2023-12-04 15:28:36 +0800

    1.1.8

--- src/Common.php
+++ src/Common.php
@@ -1068,7 +1068,7 @@ public function downloadDocument($fileSrc, $mimetype = "application/octet-stream
  * @param {Object} $string	字符串
  * @param {Object} $params	参数
  */
-public function str($string, $params)
+public static function str($string, $params=[])
 {
     foreach ($params as $key => $value) {
         $string = preg_replace("/\{$key\}/", $value, $string);
