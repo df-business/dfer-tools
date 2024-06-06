a069841456a7adec27e008933d2e0918db6b7377
Dfer.Site <df_business@qq.com>
2024-06-06 11:48:02 +0800

    1.7.3

--- src/TpCommon.php
+++ src/TpCommon.php
@@ -1,7 +1,11 @@
 <?php
+
 /**
  * +----------------------------------------------------------------------
  * | thinkphp常用的方法
+ * | 对于tp2、3、5、6、8这种大版本迭代，许多功能的开发需要区分对待
+ * | tp>=5  https://github.com/top-think/think    https://github.com/top-think/framework    开始支持composer加载框架，以及控制台think命令
+ * | tp<5   https://github.com/top-think/thinkphp     只能手动复制框架文件
  * +----------------------------------------------------------------------
  *                                            ...     .............
  *                                          ..   .:!o&*&&&&&ooooo&; .
@@ -31,69 +35,81 @@
  * +----------------------------------------------------------------------
  *
  */
+
 namespace Dfer\Tools;
 
-use think\Validate;
+use think\{Validate, Db};
 
-class TpCommon
+class TpCommon extends Common
 {
-    protected static $db;
-    protected static $tp_ver;
-    protected static $tp_new;
+    // tp版本
+    protected $tp_version;
+
     public function __construct()
     {
-        // tp5与tp6调用方式不同
-        if (class_exists("\\think\\facade\\Db")) {
-            // tp6
-            self::$tp_new=true;
-            self::$tp_ver=app()->version();
-            self::$db = new \think\facade\Db();
-        } else {
-            // tp5以下
-            self::$tp_new=false;
-            self::$tp_ver=THINK_VERSION;
-            self::$db = new \think\Db();
-        }
+        $this->tp_version = $this->getVersion();
     }
 
-     /**
-      * 获取表的字段信息
-      * @param {Object} $table 表名
-      * @param {Object} keys 返回的字段。支持数组，字符串，为空则返回所有字段
-      * @param {Object} $col_name    获取的字段属性，默认是备注
-      */
-    public function getColName($table,$keys=[], $col_name='Comment')
+    /**
+     * 获取表的字段信息
+     * @param {Object} $table 表名
+     * @param {Object} keys 返回的字段。支持数组，字符串，为空则返回所有字段
+     * @param {Object} $col_name    获取的字段属性，默认是备注
+     */
+    public function getColName($table, $keys = [], $col_name = 'Comment')
     {
-        $list=self::$db::query("SHOW FULL COLUMNS FROM {$table};");
-
-        $item=[];
-        foreach ($list as $key => $value) {
-            $item[$value['Field']]=$value[$col_name];
+        if ($this->facade()) {
+            \think\facade\Db::query("SHOW FULL COLUMNS FROM {$table};");
+        } else {
+            $list = (new Db)->query("SHOW FULL COLUMNS FROM {$table};");
         }
 
-        if(is_array($keys)){
-            if(count($keys)==0){
+        $item = [];
+        foreach ($list as $key => $value) {
+            $item[$value['Field']] = $value[$col_name];
+        }
+
+        if (is_array($keys)) {
+            if (count($keys) == 0) {
                 return $item;
             }
 
-            foreach($keys as $key=>$value){
-            $data[]=$item[$value];
+            foreach ($keys as $key => $value) {
+                $data[] = $item[$value];
             }
-        }
-        else{
-            $data=$item[$keys];
+        } else {
+            $data = $item[$keys];
         }
         return $data;
     }
 
     /**
-     * 独立日志
-     *
-     * 'apart_level'=>['error','sql','debug','dfer']
+     * 静态调用
+     * tp>=5.1 开始支持facade
+     * @param {Object} $require 必须支持静态调用
      **/
-    public function log($data, $identification='dfer')
+    public function facade($require = false)
     {
-        if (class_exists("\\think\\facade\\Log")) {
+        $need_ver = "5.1";
+        if ($require) {
+            version_compare($this->tp_version, $need_ver, '>=') or die("需要 ThinkPHP >= v{$need_ver} !");
+        }
+
+        if ($this->tp_version >= $need_ver) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 日志
+     * 独立日志：'apart_level'=>['error','sql','debug','dfer']
+     *
+     **/
+    public function log($data, $identification = 'dfer')
+    {
+        if ($this->facade()) {
             \think\facade\Log::write($data, $identification);
         } else {
             \think\Log::write($data, $identification);
@@ -107,7 +123,9 @@ class TpCommon
      */
     public function uploadForm()
     {
-        $files=request()->file();
+        $this->facade(true);
+
+        $files = request()->file();
         // dump($files);
         if (!empty($files)) {
             try {
@@ -117,7 +135,7 @@ class TpCommon
                     echo $validate->getError();
                 }
 
-                $files=$files['image'];
+                $files = $files['image'];
                 // 多文件上传
                 if (\is_array($files)) {
                     $savename = [];
@@ -180,7 +198,29 @@ class TpCommon
         }
 
         return $result;
-
     }
 
+    /**
+     * 获取tp版本
+     * @param {Object} $var 变量
+     **/
+    public function getVersion($var = null)
+    {
+        // 初始版本
+        $tp_version = "0.0.0";
+
+        // 优先通过tp内置方法来获取版本号。
+        if (function_exists('app') && method_exists(app(), 'version')) {
+            // 从tp5开始支持。从tp8开始，通过composer获取更精确的版本号
+            $tp_version = app()->version();
+        } elseif (class_exists('\think\App') && defined('\think\App::VERSION')) {
+            // 从tp5开始支持，每代tp的App类都会携带VERSION常量
+            $tp_version = \think\App::VERSION;
+        } elseif (defined('THINK_VERSION')) {
+            // 从tp2开始支持，到tp5被移除
+            $tp_version = THINK_VERSION;
+        }
+        // var_dump($tp_version,$tp_version>="5.1.0",class_exists('\think\App'),defined('\think\App::VERSION'),function_exists('app')&&method_exists(app(), 'version'));
+        return $tp_version;
+    }
 }
